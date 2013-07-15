aa6bc884f324ca52002f98572cd8e69353db66a6
Suki <suki@missallsunday.com>
2013-07-15 16:00:35 -0500

    Initial commit
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ README.md
@@ -0,0 +1,6 @@
+
+The software is licensed under [MPL 1.1 license](http://www.mozilla.org/MPL/MPL-1.1.html).
+
+######Expire Attachments:
+
+A simple modification to allow users to set a predefined date for their attachments.
---
+++ install.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * @package Expire Attachments mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+	if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+		require_once(dirname(__FILE__) . '/SSI.php');
+
+	elseif (!defined('SMF'))
+		exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
+	global $smcFunc, $context;
+
+	db_extend('packages');
+
+	if (empty($context['uninstalling']))
+	{
+
+	}

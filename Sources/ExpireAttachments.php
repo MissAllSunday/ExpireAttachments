112e1970d274eb6bb444345ec2e58c1444610493
Suki <suki@missallsunday.com>
2013-07-19 10:34:15 -0500

    Thank you Activity Bar for the nice example on how to use $config_vars, its been a while...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/ExpireAttachments.php
+++ Sources/ExpireAttachments.php
@@ -1,1 +1,30 @@
-<?php/** * * @package Expire Attachments mod * @version 1.0 * @author Jessica González <suki@missallsunday.com> * @copyright Copyright (c) 2013, Jessica González * @license http://www.mozilla.org/MPL/MPL-1.1.html */if (!defined('SMF'))	die('No direct access...');class ExpireAttachments{	public static $name = 'ExpireAttachments';	public static $shortName = 'ExAt';	public static function Activity_Bar_settings(&$config_vars)	{		global $txt;		if (!isset($txt['ExAt_modName']))			loadLanguage(__CLASS__);		$config_vars[] = $txt['Activity_Bar_title'];		$config_vars[] = array('check', 'Activity_Bar_enable');		$config_vars[] = array('check', 'Activity_Bar_show_in_posts');		$config_vars[] = array('check', 'Activity_Bar_show_in_profile');		$config_vars[] = array('text', 'Activity_Bar_label');		$config_vars[] = array('int', 'Activity_Bar_timeframe');		$config_vars[] = array('int', 'Activity_Bar_max_posts');		$config_vars[] = array('int', 'Activity_Bar_max_width');		$config_vars[] = '';	}}
+<?php
+
+/**
+ *
+ * @package Expire Attachments mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class ExpireAttachments
+{
+	public static $name = 'ExpireAttachments';
+	public static $shortName = 'ExAt';
+
+	public static function Activity_Bar_settings(&$config_vars)
+	{
+		global $txt;
+
+		if (!isset($txt['ExAt_modName']))
+			loadLanguage(__CLASS__);
+
+		$config_vars[] = $txt['ExAt_modName'];
+		$config_vars[] = '';
+	}
+}

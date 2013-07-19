576aa5df18e8abca1857da443637c427da8e16c6
Suki <suki@missallsunday.com>
2013-07-19 10:31:51 -0500

    Language file and a basic settings page
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Themes/default/languages/ExpireAttachments.english.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * @package Expire Attachments mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+ 
+global $txt;
+
+$txt['ExAt_modName'] = 'Expire Attachments mod';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';
+$txt['ExAt_'] = '';

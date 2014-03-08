065267e3febd79d10be8685a1b9ab02b722ca589
Suki <suki@missallsunday.com>
2014-03-08 10:35:31 -0600

    utf8 language file and minor typos
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/ExpireAttachments.english-utf8.php
+++ Themes/default/languages/ExpireAttachments.english-utf8.php
@@ -4,8 +4,8 @@
  *
  * @package Expire Attachments mod
  * @version 1.0
- * @author Jessica Gonz·lez <suki@missallsunday.com>
- * @copyright Copyright (c) 2013, Jessica Gonz·lez
+ * @author Jessica Gonz√°lez <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica Gonz√°lez
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
 

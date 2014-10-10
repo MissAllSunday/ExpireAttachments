788fea7b32f07fbf17819d239b1b1dc5e170fdd8
Suki <suki@missallsunday.com>
2014-10-10 11:12:05 -0500

    Move the code for removing the scheduled task to its own file and execute it everytime theres an uninstall.
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ removeScheduledTask.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * @package Expire Attachments mod
+ * @version 1.0
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2013, Jessica González
+ * @license http://www.mozilla.org/MPL/MPL-1.1.html
+ */
+ 
+if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
+	require_once(dirname(__FILE__) . '/SSI.php');
+
+elseif (!defined('SMF'))
+	exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
+
+global $smcFunc;
+
+db_extend('packages');
+
+$smcFunc['db_query']('', '
+	DELETE FROM {db_prefix}scheduled_tasks
+	WHERE task = {string:name}',
+	array(
+		'name' => 'attachments_expire_date',
+	)
+);
